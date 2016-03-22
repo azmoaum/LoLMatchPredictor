5601457565e307de03da7425a305230749fc62eb
Aodh Zhao <aodhzhao@gmail.com>
2016-03-21 17:25:51 -0700

    Modified features to use the difference of team 1 and team 2. Adding cross validation

--- sandbox/svm.py
+++ sandbox/svm.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn import cross_validation
 
 def formatData(data, participantInfo):
     X = [] #average KDA, average winrate, average masteryLvl
@@ -13,6 +14,7 @@ def formatData(data, participantInfo):
     for tu in data:
         matchId = tu[0]
         win1 = tu[9]
+        win2 = tu[9+(8*16)]
 
         participants = []
         participants.append(participantInfo[(counter*10)])
@@ -21,12 +23,24 @@ def formatData(data, participantInfo):
         participants.append(participantInfo[(counter*10) + 3])
         participants.append(participantInfo[(counter*10) + 4])
 
+        participants.append(participantInfo[(counter*10) + 5])
+        participants.append(participantInfo[(counter*10) + 6])
+        participants.append(participantInfo[(counter*10) + 7])
+        participants.append(participantInfo[(counter*10) + 8])
+        participants.append(participantInfo[(counter*10) + 9])
+
         #Team 1
-        kda1, winrate1, masteryLvl1 = getAverages(participants)
+        kda1, winrate1, masteryLvl1 = getAverages(participants[0:5])
+
+        #Team 2
+        kda2, winrate2, masteryLvl2 = getAverages(participants[5:10])
     
-        X.append((kda1, winrate1, masteryLvl1))
+        #X.append((kda1, winrate1, masteryLvl1))
+        #X.append((kda2, winrate2, masteryLvl2))
+        #Y.append(win1)
+        #Y.append(win2)
+        X.append((kda1-kda2, winrate1-winrate2, masteryLvl1-masteryLvl2))
         Y.append(win1)
-        
         counter = counter + 1
 			
     return X,Y
@@ -46,11 +60,11 @@ def main():
     dataset = np.recfromcsv("matches.csv", delimiter=",")
     participantInfo = np.recfromcsv("participantInfo.csv", delimiter=",")
     
-    trainDataEnd = 700
-    testDataEnd = 900
+    #trainDataEnd = 700
+    #testDataEnd = 900
     
-    XTrain, YTrain = formatData(dataset[0:trainDataEnd], participantInfo)
-    
+    X, Y = formatData(dataset, participantInfo)
+    """
     clf = svm.SVC()
     clf.fit(XTrain, YTrain)
         
@@ -61,6 +75,24 @@ def main():
         YPredicted.append(clf.predict([list(x)]))
     
     print accuracy_score(YTest, YPredicted)
+"""
+    svc = svm.SVC(kernel='linear', C=1)
+    scores = cross_validation.cross_val_score(svc, X, Y, cv=10)
+    print scores.mean()
+    """
+    X_folds = np.array_split(X, 10)
+    y_folds = np.array_split(Y, 10)
+    scores = list()
+    for k in range(10):
+        # We use 'list' to copy, in order to 'pop' later on
+        X_train = list(X_folds)
+        X_test  = X_train.pop(k)
+        X_train = np.concatenate(X_train)
+        y_train = list(y_folds)
+        y_test  = y_train.pop(k)
+        y_train = np.concatenate(y_train)
+        scores.append(svc.fit(X_train, y_train).score(X_test, y_test))
+    print(scores)"""
 
 if __name__ == '__main__':
     main()
